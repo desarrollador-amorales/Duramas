8efc0adbd77c4e579cfb0cc349a32a7b576c44b8
sistemasduramas <sistemasgeneral@duramas.com.ec>
2023-06-13 17:33:28 -0500

    almacen

--- WinDuramas/FrmAlmacen.Designer.vb
+++ WinDuramas/FrmAlmacen.Designer.vb
@@ -28,7 +28,13 @@ Partial Class FrmAlmacen
         TxtCriterio = New TextBox()
         ListaDatos = New ListView()
         GroupBox1 = New GroupBox()
+        CmdSalir = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
         cmdNuevo = New Button()
+        GroupBox2 = New GroupBox()
+        GroupBox3 = New GroupBox()
         GboxListado.SuspendLayout()
         GroupBox1.SuspendLayout()
         SuspendLayout()
@@ -39,69 +45,130 @@ Partial Class FrmAlmacen
         GboxListado.Controls.Add(TxtCriterio)
         GboxListado.Controls.Add(ListaDatos)
         GboxListado.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
-        GboxListado.Location = New Point(14, 16)
-        GboxListado.Margin = New Padding(3, 4, 3, 4)
+        GboxListado.Location = New Point(12, 12)
         GboxListado.Name = "GboxListado"
-        GboxListado.Padding = New Padding(3, 4, 3, 4)
-        GboxListado.Size = New Size(457, 631)
+        GboxListado.Size = New Size(400, 473)
         GboxListado.TabIndex = 0
         GboxListado.TabStop = False
         GboxListado.Text = "Listado de Almacenes"
         ' 
         ' TxtCriterio1
         ' 
-        TxtCriterio1.Location = New Point(185, 29)
-        TxtCriterio1.Margin = New Padding(3, 4, 3, 4)
+        TxtCriterio1.Location = New Point(88, 21)
+        TxtCriterio1.Margin = New Padding(3, 2, 3, 2)
         TxtCriterio1.Name = "TxtCriterio1"
-        TxtCriterio1.Size = New Size(234, 27)
+        TxtCriterio1.Size = New Size(271, 23)
         TxtCriterio1.TabIndex = 2
         ' 
         ' TxtCriterio
         ' 
-        TxtCriterio.Location = New Point(37, 29)
-        TxtCriterio.Margin = New Padding(3, 4, 3, 4)
+        TxtCriterio.Location = New Point(18, 21)
         TxtCriterio.Name = "TxtCriterio"
-        TxtCriterio.Size = New Size(141, 27)
+        TxtCriterio.Size = New Size(64, 23)
         TxtCriterio.TabIndex = 1
         ' 
         ' ListaDatos
         ' 
-        ListaDatos.Location = New Point(17, 68)
-        ListaDatos.Margin = New Padding(3, 4, 3, 4)
+        ListaDatos.AllowColumnReorder = True
+        ListaDatos.Location = New Point(6, 50)
+        ListaDatos.Margin = New Padding(3, 2, 3, 2)
         ListaDatos.Name = "ListaDatos"
-        ListaDatos.Size = New Size(420, 537)
+        ListaDatos.Size = New Size(368, 414)
         ListaDatos.TabIndex = 0
         ListaDatos.UseCompatibleStateImageBehavior = False
+        ListaDatos.View = View.Details
         ' 
         ' GroupBox1
         ' 
+        GroupBox1.Controls.Add(CmdSalir)
+        GroupBox1.Controls.Add(Button3)
+        GroupBox1.Controls.Add(Button2)
+        GroupBox1.Controls.Add(Button1)
         GroupBox1.Controls.Add(cmdNuevo)
-        GroupBox1.Location = New Point(483, 571)
-        GroupBox1.Margin = New Padding(3, 4, 3, 4)
+        GroupBox1.Location = New Point(423, 428)
         GroupBox1.Name = "GroupBox1"
-        GroupBox1.Padding = New Padding(3, 4, 3, 4)
-        GroupBox1.Size = New Size(414, 76)
+        GroupBox1.Size = New Size(362, 57)
         GroupBox1.TabIndex = 1
         GroupBox1.TabStop = False
+        ' 
+        ' CmdSalir
+        ' 
+        CmdSalir.Image = CType(resources.GetObject("CmdSalir.Image"), Image)
+        CmdSalir.Location = New Point(295, 13)
+        CmdSalir.Margin = New Padding(3, 2, 3, 2)
+        CmdSalir.Name = "CmdSalir"
+        CmdSalir.Size = New Size(47, 39)
+        CmdSalir.TabIndex = 4
+        CmdSalir.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Image = CType(resources.GetObject("Button3.Image"), Image)
+        Button3.Location = New Point(231, 13)
+        Button3.Margin = New Padding(3, 2, 3, 2)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(47, 39)
+        Button3.TabIndex = 3
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Image = CType(resources.GetObject("Button2.Image"), Image)
+        Button2.Location = New Point(162, 13)
+        Button2.Margin = New Padding(3, 2, 3, 2)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(47, 39)
+        Button2.TabIndex = 2
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Image = CType(resources.GetObject("Button1.Image"), Image)
+        Button1.Location = New Point(98, 13)
+        Button1.Margin = New Padding(3, 2, 3, 2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(47, 39)
+        Button1.TabIndex = 1
+        Button1.UseVisualStyleBackColor = True
         ' 
         ' cmdNuevo
         ' 
         cmdNuevo.Image = CType(resources.GetObject("cmdNuevo.Image"), Image)
-        cmdNuevo.Location = New Point(7, 16)
-        cmdNuevo.Margin = New Padding(3, 4, 3, 4)
+        cmdNuevo.Location = New Point(31, 13)
+        cmdNuevo.Margin = New Padding(3, 2, 3, 2)
         cmdNuevo.Name = "cmdNuevo"
-        cmdNuevo.Size = New Size(54, 52)
+        cmdNuevo.Size = New Size(47, 39)
         cmdNuevo.TabIndex = 0
         cmdNuevo.UseVisualStyleBackColor = True
         ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        GroupBox2.Location = New Point(423, 12)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(365, 410)
+        GroupBox2.TabIndex = 2
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Datos del Almacén"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.ForeColor = Color.FromArgb(CByte(0), CByte(0), CByte(192))
+        GroupBox3.Location = New Point(423, 12)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(363, 410)
+        GroupBox3.TabIndex = 2
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Datos del Almacén"
+        ' 
         ' FrmAlmacen
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(914, 673)
+        ClientSize = New Size(800, 505)
+        Controls.Add(GroupBox3)
         Controls.Add(GroupBox1)
         Controls.Add(GboxListado)
-        Margin = New Padding(3, 4, 3, 4)
         MaximizeBox = False
         MinimizeBox = False
         Name = "FrmAlmacen"
@@ -118,4 +185,10 @@ Partial Class FrmAlmacen
     Friend WithEvents TxtCriterio As TextBox
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents cmdNuevo As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CmdSalir As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox3 As GroupBox
 End Class
--- WinDuramas/FrmAlmacen.vb
+++ WinDuramas/FrmAlmacen.vb
@@ -1,10 +1,32 @@
 ﻿Imports System.Collections.ObjectModel
 Imports System.Data.SqlClient
+Imports System.Runtime.CompilerServices
+Imports System.Security.Principal
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
 Public Class FrmAlmacen
+    Dim Nuevo As Integer
+    Dim Estado As String
+
+    Sub EnceraPantalla()
+
+        TxtCriterio.Text = ""
+        TxtCriterio1.Text = ""
+        '        cmdEliminar.Enabled = False
+        Nuevo = 1
+    End Sub
+
     Private Sub FrmAlmacen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        '        MDIPrincipal.MnuOpcion4(0).Enabled = False
+        '        Me.Left = ((MDIPrincipal.Width - MDIPrincipal.Picture1.Width - Me.Width) / 2)
+        '        Me.Top = ((MDIPrincipal.Height - Me.Height) / 2) - 500
+        ListaDatos.Columns.Add("Almacén", 80, 0)
+        ListaDatos.Columns.Add("Descripción", 250, 0)
+        ListaDatos.Alignment = 0
         ListadoAlmacenes()
+        EnceraPantalla()
     End Sub
 
     Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GboxListado.Enter
@@ -26,21 +48,17 @@ Public Class FrmAlmacen
     End Sub
 
     Sub ListadoAlmacenes()
-        'nueva consulta
-        '  Dim Tabla As New Sql
-        ' nuevo comentario
-        'linea
         Dim Tabla As New ADODB.Recordset
-        Dim Campo As String
+        Dim ItmX As ListViewItem
 
-        Tabla = ControlAlmacenes.ObtieneAlmacenes("", "")
+        Tabla = ControlAlmacenes.ObtieneAlmacenes(TxtCriterio.Text, TxtCriterio1.Text)
 
-        '        ListaDatos.ListItems.Clear
+        ListaDatos.Items.Clear()
         Do While Not Tabla.EOF
-            '           Set itmX = ListaDatos.ListItems.Add(, , CStr(Tabla.Fields("almacencod")))
-            '            itmX.SubItems(1) = CStr(Tabla.Fields("almacendes"))
-            Campo = Tabla.Fields("almacendes").Value
-            MessageBox.Show(Tabla.Fields("almacendes").Value)
+            ItmX = ListaDatos.Items.Add(CStr(Tabla.Fields("almacencod").Value))
+            ItmX.SubItems.Add(CStr(Tabla.Fields("almacendes").Value))
+            '            ItmX.SubItems.Add(CStr(Tabla.Fields("almacendes")))
+
             Tabla.MoveNext()
         Loop
 
@@ -50,7 +68,19 @@ Public Class FrmAlmacen
 
     End Sub
 
-    Private Sub TxtCriterio1_TextChanged(sender As Object, e As EventArgs) Handles TxtCriterio1.TextChanged
+    Private Sub TxtCriterio_TextChanged(sender As Object, e As EventArgs) Handles TxtCriterio.TextChanged
+        ListadoAlmacenes()
+    End Sub
 
+    Private Sub TxtCriterio1_TextChanged(sender As Object, e As EventArgs) Handles TxtCriterio1.TextChanged
+        ListadoAlmacenes()
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+
+    Private Sub CmdSalir_Click(sender As Object, e As EventArgs) Handles CmdSalir.Click
+        Close()
     End Sub
 End Class
